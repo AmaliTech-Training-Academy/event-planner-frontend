e182621d2f47a73b7d4ffb4025292647c3226cfb
Sule Malik <abdulmaliksuleman75@gmail.com>
2025-10-17 15:47:56 +0000

    refactor(checkbox): add explicit access modifiers and use signal input/output variants

--- src/app/shared/ui/checkbox/checkbox.component.ts
+++ src/app/shared/ui/checkbox/checkbox.component.ts
@@ -7,18 +7,18 @@ import { Component, input, output, signal } from '@angular/core';
   styleUrls: ['./checkbox.component.scss'],
 })
 export class CheckboxComponent {
-  label = input<string>('');
-  checked = input<boolean>(false);
+  public label = input<string>('');
+  public checked = input<boolean>(false);
 
-  checkedChange = output<boolean>();
+  public checkedChange = output<boolean>();
 
-  public _checked = signal(this.checked());
+  protected _checked = signal(this.checked());
 
-  get isChecked(): boolean {
+  public get isChecked(): boolean {
     return this._checked();
   }
 
-  toggle(): void {
+  public toggle(): void {
     this._checked.update((current) => !current);
     this.checkedChange.emit(this._checked());
   }
