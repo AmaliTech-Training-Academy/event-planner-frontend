f131777329545f888bbae58974aca37fb586f6c7
Esihackman <mabelabenahackman602@gmail.com>
2025-10-21 15:21:14 +0000

    refactor(forgot-password): replace local button with reusable app-button component for consistency and style reuse

--- src/app/shared/ui/button/button.component.ts
+++ src/app/shared/ui/button/button.component.ts
@@ -1,19 +1,16 @@
+
+
 import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss'],
-  standalone: true,
+  styleUrls: ['./button.component.scss']  
 })
+
+
 export class ButtonComponent {
-_disabled() {
-throw new Error('Method not implemented.');
-}
-_fullWidth() {
-throw new Error('Method not implemented.');
-}
-  public readonly type = input<'primary' | 'social'>('primary');
-  public readonly disabled = input(false);
-  public readonly fullWidth = input(false);
+  disabled = input<boolean>(false); 
+fullWidth = input<boolean>(false); 
+type = input<'primary' | 'social'>('primary'); 
 }
