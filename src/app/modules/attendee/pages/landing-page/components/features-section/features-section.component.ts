5ea3f8a3b3bf10bc329d07f210af764f422e9de6
Sule Malik <abdulmaliksuleman75@gmail.com>
2025-10-24 08:40:11 +0000

    chore: removed signals from static values on the feature-section component

--- src/app/modules/attendee/pages/landing-page/components/features-section/features-section.component.ts
+++ src/app/modules/attendee/pages/landing-page/components/features-section/features-section.component.ts
@@ -13,7 +13,7 @@ export class FeaturesSectionComponent {
   protected readonly title: string = 'Manage & Collaborate On Events';
 
   protected readonly description: string =
-    'EventUp is designed to be inclusive, by being able to use your own device to help hybrid-conference teams create, collaborate and celebrate together.';
+    'EventUp is designed to be inclusive, by being able to use your own device to help hybrid conference teams create, collaborate and celebrate together.';
 
   protected readonly dashboardImage: string = 'images/card-img.png';
 }
