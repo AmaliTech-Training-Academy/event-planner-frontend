9303b755f999caf54b42992290a5d58d88f61314
Sule Malik <abdulmaliksuleman75@gmail.com>
2025-11-03 15:44:08 +0000

    feat(admin-login-integration): set up endpoints to call admin-login api

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -13,82 +13,97 @@ export class AuthService {
   private _loadingStateSubject = new BehaviorSubject<boolean>(false);
   public readonly loading$ = this._loadingStateSubject.asObservable();
 
-  constructor(private readonly authBackend: AuthBackendService, private readonly router: Router, private readonly errorHandlerService: ErrorHandlerService) { }
+  constructor(
+    private readonly authBackend: AuthBackendService,
+    private readonly router: Router,
+    private readonly errorHandlerService: ErrorHandlerService
+  ) {}
 
   public login(email: string, password: string) {
     this.setLoading(true);
-    return this.authBackend.login(email, password)
-      .pipe(
-        tap(() => {
-          this.router.navigate([APP_ROUTES.VERIFY_EMAIL], {
-            queryParams: { email },
-          });
-        }),
-        catchError(err => this.errorHandlerService.handle(err)),
-        finalize(() => this.setLoading(false))
-      );
+    return this.authBackend.login(email, password).pipe(
+      tap(() => {
+        this.router.navigate([APP_ROUTES.VERIFY_EMAIL], {
+          queryParams: { email },
+        });
+      }),
+      catchError((err) => this.errorHandlerService.handle(err)),
+      finalize(() => this.setLoading(false))
+    );
+  }
+  public adminLogin(email: string, password: string) {
+    this.setLoading(true);
+    return this.authBackend.adminLogin(email, password).pipe(
+      tap(() => {
+        this.router.navigate([APP_ROUTES.ADMIN_DASHBOARD]);
+      }),
+      catchError((err) => this.errorHandlerService.handle(err)),
+      finalize(() => this.setLoading(false))
+    );
   }
 
-  public register(fullName: string, email: string, password: string, confirmPassword: string) {
+  public register(
+    fullName: string,
+    email: string,
+    password: string,
+    confirmPassword: string
+  ) {
     this.setLoading(true);
-    return this.authBackend.register(fullName, email, password, confirmPassword)
+    return this.authBackend
+      .register(fullName, email, password, confirmPassword)
       .pipe(
         tap(() => {
           this.router.navigate([APP_ROUTES.LOGIN], {
             queryParams: { email },
           });
-        }), catchError(err => this.errorHandlerService.handle(err)),
+        }),
+        catchError((err) => this.errorHandlerService.handle(err)),
         finalize(() => this.setLoading(false))
       );
   }
 
-
   public verifyEmail(otp: string, email: string) {
     this.setLoading(true);
-    return this.authBackend.verifyEmail(otp, email)
-      .pipe(
-        tap((response) => {
-          this._loggedIn$.next(true);
-          this.router.navigate([APP_ROUTES.LANDING_PAGE]);
-        }),
-        catchError(err => this.errorHandlerService.handle(err)),
-        finalize(() => this.setLoading(false))
-      )
+    return this.authBackend.verifyEmail(otp, email).pipe(
+      tap((response) => {
+        this._loggedIn$.next(true);
+        this.router.navigate([APP_ROUTES.LANDING_PAGE]);
+      }),
+      catchError((err) => this.errorHandlerService.handle(err)),
+      finalize(() => this.setLoading(false))
+    );
   }
 
   public logout() {
     this.setLoading(true);
-    return this.authBackend.logout()
-      .pipe(
-        tap(() => {
-          this._loggedIn$.next(false);
-          this._userInfo$.next(null);
-          this.router.navigate([APP_ROUTES.LOGIN]);
-        }),
-        catchError(err => this.errorHandlerService.handle(err)),
-        finalize(() => this.setLoading(false))
-      );
+    return this.authBackend.logout().pipe(
+      tap(() => {
+        this._loggedIn$.next(false);
+        this._userInfo$.next(null);
+        this.router.navigate([APP_ROUTES.LOGIN]);
+      }),
+      catchError((err) => this.errorHandlerService.handle(err)),
+      finalize(() => this.setLoading(false))
+    );
   }
 
   public checkAuthUser(userId: string) {
     this.setLoading(true);
-    return this.authBackend.checkAuthUser(userId)
-      .pipe(
-        tap(response => {
-          this._userInfo$.next(response?.data)
-        }),
-        catchError(err => this.errorHandlerService.handle(err)),
-        finalize(() => this.setLoading(false))
-      );
+    return this.authBackend.checkAuthUser(userId).pipe(
+      tap((response) => {
+        this._userInfo$.next(response?.data);
+      }),
+      catchError((err) => this.errorHandlerService.handle(err)),
+      finalize(() => this.setLoading(false))
+    );
   }
 
   public forgotPassword(email: string) {
     this.setLoading(true);
-    return this.authBackend.forgotPassword(email)
-      .pipe(
-        catchError(err => this.errorHandlerService.handle(err)),
-        finalize(() => this.setLoading(false))
-      )
+    return this.authBackend.forgotPassword(email).pipe(
+      catchError((err) => this.errorHandlerService.handle(err)),
+      finalize(() => this.setLoading(false))
+    );
   }
 
   public isLoggedIn(): Observable<boolean> {
@@ -96,11 +111,10 @@ export class AuthService {
   }
 
   public currentUser(): User | null {
-    return this._userInfo$.getValue()
+    return this._userInfo$.getValue();
   }
 
   private setLoading(isLoading: boolean): void {
     this._loadingStateSubject.next(isLoading);
   }
-
 }
