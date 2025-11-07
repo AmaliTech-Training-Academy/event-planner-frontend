d581b4f664679466aed2ec604b2d22e88416a113
penielny <penielnyinaku@gmail.com>
2025-11-07 00:53:11 +0000

    Refactor About Page: Replace static content with components for missions, team, and story sections

--- src/app/modules/attendee/pages/about-page/about-page.component.ts
+++ src/app/modules/attendee/pages/about-page/about-page.component.ts
@@ -1,82 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { APP_ROUTES } from '../../../../core/constants/app-routes.constants';
+import { PartnersComponent } from "../../../../shared/components/partners/partners.component";
+import { ABOUT_MISSIONS, ABOUT_OUR_STORY, ABOUT_TEAM_MEMBERS } from './constants/about.constant';
+import { SteperComponent } from "./components/steper/steper.component";
+import { TeamComponent } from "./components/team/team.component";
+import { MissionComponent } from "./components/mission/mission.component";
+import { ButtonComponent } from "../../../../shared/ui/button/button.component";
 
 @Component({
   selector: 'app-about-page',
-  imports: [RouterModule],
+  imports: [RouterModule, PartnersComponent, SteperComponent, TeamComponent, MissionComponent, ButtonComponent],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss'
 })
 export class AboutPageComponent {
 
-  protected readonly missions: {
-    title: string,
-    description: string;
-    icon: string;
-  }[] = [
-      {
-        title: 'Innovation',
-        description: 'Constantly pushing boundaries to create cutting-edge event experiences.',
-        icon: "icons/bulb.svg"
-      },
-      {
-        title: 'Collaboration',
-        description: 'Fostering meaningful connections between people across the globe.',
-        icon: "icons/social.svg"
-      },
-      {
-        title: 'Reliability',
-        description: 'Delivering consistent, high-quality experiences you can count on.',
-        icon: "icons/drop.svg"
-      },
-      {
-        title: 'Growth',
-        description: 'Empowering continuous learning and development for all users.',
-        icon: "icons/trend_down.svg"
-      }
-    ]
-  protected readonly teamMembers: {
-    name: string;
-    role: string;
-    image: string;
-    description: string;
-  }[] = [
-      {
-        name: 'Johnson Sam',
-        role: 'CEO & Founder',
-        image: 'images/team-member1.jpg',
-        description: 'Former event director with 15+ years experience in creating memorable experiences. '
-      },
-      {
-        name: 'Michael Chen',
-        role: 'CTO',
-        image: 'images/team-member2.jpg',
-        description: 'Tech visionary specializing in scalable platforms and user experience design.'
-      },
-      {
-        name: 'Emily Rodriguez',
-        role: 'Head of Marketing',
-        image: 'images/team-member4.jpg',
-        description: 'Creative strategist passionate about connecting brands with their audiences.'
-      },
-      {
-        name: 'David Kim',
-        role: 'Lead Developer',
-        image: 'images/team-member3.jpg',
-        description: 'Full-stack engineer focused on building robust and intuitive event solutions.'
-      },
-      {
-        name: 'Lisa Thompson',
-        role: 'Customer Success',
-        image: 'images/team-member6.jpg',
-        description: 'Dedicated to ensuring every client achieves their event goals and beyond.'
-      },
-      {
-        name: 'James Wilson',
-        role: 'Product Designer',
-        image: 'images/team-member5.jpg',
-        description: 'UX expert crafting intuitive interfaces that make event management effortless.'
-      }
-    ]
+  constructor(private readonly router: Router) { }
+  protected readonly APP_ROUTE = APP_ROUTES;
+
+  protected goTo(path: string) {
+    this.router.navigate([path])
+  }
 
 }
