40b0cc1650a09fe95cc730dff4c780db425521e5
Esihackman <mabelabenahackman602@gmail.com>
2025-11-13 11:44:24 +0000

    Fix: Removed all mock data and replaced with the todo API call comments

--- src/app/modules/attendee/pages/my-events-page/my-events-page.component.ts
+++ src/app/modules/attendee/pages/my-events-page/my-events-page.component.ts
@@ -8,11 +8,7 @@ import { EventCard } from '../../../../core/models/event.model';
 import { UserCardData } from '../../../../core/models/user.model';
 import { LineSeriesConfig } from '../../../../core/models/chart.model';
 import {ButtonComponent} from "../../../../shared/ui/button/button.component";
-import {
-  MOCK_MY_EVENTS_CARDS,
-  MOCK_STAT_CARDS,
-  MOCK_CHART_SERIES,
-} from '../../../../core/data/mock-data';
+
 import { APP_ROUTES } from '../../../../core/constants/app-routes.constants';
 
 @Component({
@@ -33,20 +29,17 @@ export class MyEventsPageComponent implements OnInit {
   private readonly router = inject(Router);
   protected readonly routes = APP_ROUTES;
 
+  // TODO: Replace with API call to fetch user's events
   protected myEvents = signal<EventCard[]>([]);
+  
+  // TODO: Replace with API call to fetch user's stats
   protected statCards = signal<UserCardData[]>([]);
+  
+  // TODO: Replace with API call to fetch user's chart data
   protected chartSeries = signal<LineSeriesConfig[]>([]);
   
   public ngOnInit(): void {
     
-    // TODO: Replace with API call to fetch user's events
-    this.myEvents.set(MOCK_MY_EVENTS_CARDS);
-    
-    // TODO: Replace with API call to fetch user's stats
-    this.statCards.set(MOCK_STAT_CARDS);
-    
-    // TODO: Replace with API call to fetch user's chart data
-    this.chartSeries.set(MOCK_CHART_SERIES);
   }
 
   protected onManageEvent(event: EventCard): void {
