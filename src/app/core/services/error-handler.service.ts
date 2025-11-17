ffc14170aeb50b752dd323a5e55b45fec811c970
penielny <penielnyinaku@gmail.com>
2025-11-17 11:22:36 +0000

    chore: add access modifiers and remove console log from services

--- src/app/core/services/error-handler.service.ts
+++ src/app/core/services/error-handler.service.ts
@@ -14,10 +14,6 @@ export class ErrorHandlerService {
 
   handle(error: HttpErrorResponse) {
     let message = 'An unknown error occurred';
-    
-    console.log(error.error?.description)
-
-    // console.log(error.error)
 
     if (error.error instanceof ErrorEvent) {
       message = `Network error: ${error.error.message}`;
