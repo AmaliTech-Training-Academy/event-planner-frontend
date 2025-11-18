729803b89a95196ee081a603b1806a80ca01a80f
Esihackman <mabelabenahackman602@gmail.com>
2025-11-18 18:01:16 +0100

    feat:Created a deactivated modal

--- src/app/core/constants/app-routes.constants.ts
+++ src/app/core/constants/app-routes.constants.ts
@@ -14,5 +14,6 @@ export const APP_ROUTES = {
   CREATE_EVENT: '/app/create-event',
   CREATE_EVENT_SUCCESS: '/app/create-event-success',
   ADMIN_DASHBOARD: '/admin',
+  DEACTIVATED_MODAL: '/app/account-deactivated',
 };
 
