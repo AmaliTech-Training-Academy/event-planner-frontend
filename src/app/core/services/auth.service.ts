3da00b93eb2d0da9d96a825b6272bcdd56814317
penielny <penielnyinaku@gmail.com>
2025-11-19 16:15:17 +0000

    bug fix :  impliment periodic refresh token request

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -25,6 +25,8 @@ export class AuthService {
   private _email: string = '';
   private _otp: string = '';
   private _isResset: boolean = false;
+  private refreshTimer: ReturnType<typeof setTimeout> | null = null;
+  private TOKEN_REFRESH_INTERVAL:number = 30 as const;
 
   constructor(
     private readonly authBackend: AuthBackendService,
@@ -110,6 +112,8 @@ export class AuthService {
             userData.role
           );
         }
+        this.startAutomaticRefresh(new Date());
+
         // Route to Explore page instead
         this.router.navigate([APP_ROUTES.EXPLORE]);
       }),
@@ -164,7 +168,6 @@ export class AuthService {
       tap((response) => {
         const user = response?.data;
         if (user) {
-          // Map User to OtpBodyData
           const userData: OtpBodyData = {
             id: user.userId,
             email: user.email,
@@ -217,7 +220,6 @@ export class AuthService {
       const data = JSON.parse(stored) as AuthStorage;
       this._loggedIn$.next(data[AUTH_STORAGE.AUTHENTICATED]);
 
-      // Restore user info from storage
       if (data[AUTH_STORAGE.AUTHENTICATED]) {
         const userData: OtpBodyData = {
           id: parseInt(data[AUTH_STORAGE.USER_ID], 10),
@@ -228,7 +230,9 @@ export class AuthService {
         };
         this._userInfo$.next(userData);
         const refreshedAt = data[AUTH_STORAGE.REFRESHED_AT]
-        this.startAutomaticRefresh(refreshedAt)
+        this.refreshTimer = setTimeout(() => {
+          this.startAutomaticRefresh(refreshedAt);
+        }, 500);
       }
     }
   }
@@ -250,24 +254,35 @@ export class AuthService {
         [AUTH_STORAGE.PROFILE_PICTURE]: profilePicture,
         [AUTH_STORAGE.EMAIL]: email,
         [AUTH_STORAGE.ROLE]: role,
-        [AUTH_STORAGE.REFRESHED_AT]: refreshAt,
+        [AUTH_STORAGE.REFRESHED_AT]: refreshAt.toISOString(),
       })
     );
   }
 
-  private startAutomaticRefresh(lastRefresh: Date) {
+  private startAutomaticRefresh(lastRefresh: Date | string) {
+    if (this.refreshTimer) {
+      clearTimeout(this.refreshTimer);
+    }
+
+    const lastRefresh_ = new Date(lastRefresh);
+
+    if (!lastRefresh_ || isNaN(lastRefresh_.getTime())) {
+      return this.refreshToken();
+    }
+
     const now = new Date();
-    const refreshIntervalMs = 15 * 60 * 1000;
-    const nextRefreshTime = new Date(lastRefresh.getTime() + refreshIntervalMs);
+    const refreshIntervalMs = this.TOKEN_REFRESH_INTERVAL * 60 * 1000;
+    const nextRefreshTime = new Date(lastRefresh_.getTime() + refreshIntervalMs);
+
     const delay = nextRefreshTime.getTime() - now.getTime();
 
     if (delay <= 0) {
       this.refreshToken();
     } else {
-      setTimeout(() => this.refreshToken(), delay);
+      this.refreshTimer = setTimeout(() => this.refreshToken(), delay);
     }
-
   }
+
 
 
   private refreshToken() {
@@ -278,15 +293,18 @@ export class AuthService {
     const data = JSON.parse(stored) as AuthStorage;
 
     this.authBackend.refreshToken().pipe(take(1)).subscribe({
-      next: () => {
+      next: (response) => {
+        console.log(response, "token")
         this.saveAuthToStorage(
-          data[AUTH_STORAGE.USER_ID], 
-          data[AUTH_STORAGE.FULL_NAME], 
-          data[AUTH_STORAGE.PROFILE_PICTURE], 
-          data[AUTH_STORAGE.EMAIL], 
-          data[AUTH_STORAGE.ROLE], 
+          data[AUTH_STORAGE.USER_ID],
+          data[AUTH_STORAGE.FULL_NAME],
+          data[AUTH_STORAGE.PROFILE_PICTURE],
+          data[AUTH_STORAGE.EMAIL],
+          data[AUTH_STORAGE.ROLE],
           newRefreshTime
         )
+
+        this.startAutomaticRefresh(newRefreshTime);
       },
       error: () => {
         this.logout()
@@ -296,6 +314,10 @@ export class AuthService {
 
   private clearAuthStorage() {
     localStorage.removeItem(AUTH_STORAGE.AUTH);
+    if (this.refreshTimer) {
+      clearTimeout(this.refreshTimer);
+      this.refreshTimer = null;
+    }
   }
 
   public isLoggedIn(): Observable<boolean> {
